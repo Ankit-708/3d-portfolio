0284866bfef3145b2a54aac8189cdadd43b7dc0b
Ankit Vishwakarma <vishwakarmaankit708@gmail.com>
2024-02-17 19:42:48 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/3D_portfolio-main",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: "/3D_portfolio-main/"
 })
